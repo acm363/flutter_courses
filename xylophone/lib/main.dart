667c512647d78f2333fcfcbdd3074a7ad2836b5a
Churchill Atchedji <churchill.atchedji@limpidius.com>
2023-10-17 08:54:15 +0200

    feat(xylophone): add the xylophone app

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:audioplayers/audioplayers.dart';
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const XylophoneApp());
@@ -10,19 +9,46 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: TextButton(
-                onPressed: () {
-                  print('I was pressed');
-                  final player = AudioPlayer();
-                  player.play(UrlSource(
-                      "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"));
-                },
-                child: Text(adjectives.last)),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  buildXylophoneKey(color: Colors.red, soundNumber: 1),
+                  buildXylophoneKey(color: Colors.orange, soundNumber: 2),
+                  buildXylophoneKey(color: Colors.yellow, soundNumber: 3),
+                  buildXylophoneKey(color: Colors.green, soundNumber: 4),
+                  buildXylophoneKey(color: Colors.teal, soundNumber: 5),
+                  buildXylophoneKey(color: Colors.blue, soundNumber: 6),
+                  buildXylophoneKey(color: Colors.purple, soundNumber: 7),
+                ],
+              ),
+            ),
           ),
+        ));
+  }
+
+  Widget buildXylophoneKey({required Color color, required int soundNumber}) {
+
+    void playSound() {
+      final player = AudioPlayer();
+      player.play(
+        AssetSource('note$soundNumber.wav'),
+        volume: 1.0,
+      );
+    }
+
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          print('I was pressed $soundNumber');
+          playSound();
+        },
+        child: Container(
+          color: color,
+          child: Text("Play note $soundNumber"),
         ),
       ),
     );
